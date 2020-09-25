4cbba72e788237f0aa7fe52ea6287f84fa66d037
leeminhan <minhan.lmh@gmail.com>
2020-09-25 14:05:18 +0800

    Added validation for branchCode and uploaded file size

--- basic/client/my-app/src/Components/Forms.js
+++ basic/client/my-app/src/Components/Forms.js
@@ -6,13 +6,13 @@ const Forms = (props) => {
     const [validated, setValidated] = useState(false);
     const [errors, setErrors] = useState(false);
     const [file, setFile] = useState(false);
+    const DBSBranchCode = "081"
 
     const handleSubmit = (event) => {
         event.preventDefault();
         event.stopPropagation();
 
         const form = event.currentTarget;
-
         const formCustomerName = form.elements.formCustomerName.value;
         const isValidName = formCustomerName.length < 64;
         const formCustomerAge = parseInt(form.elements.formCustomerAge.value);
@@ -22,6 +22,28 @@ const Forms = (props) => {
         const formNric = form.elements.formNric.value;
         const nricRegex = new RegExp('[A-Z][0-9]{7}[A-Z]');
         const isValidNric = nricRegex.test(formNric);
+        const formRegistrationTime = form.elements.formRegistrationTime.value;
+        const formBranchCode = form.elements.formBranchCode.value;
+        const isValidBranchCode = formBranchCode ==  parseInt(DBSBranchCode);
+        const formUploadedFile = form.elements.formUploadedFile.value;
+        const isValidFileSize = ValidFileSize(formUploadedFile);
+        const formProductType = form.elements.formProductType.value;
+
+        const ValidFileSize = (fi) => { 
+            // Check if any file is selected. 
+            if (fi.files.length > 0) { 
+                for (const i = 0; i <= fi.files.length - 1; i++) { 
+                    const fsize = fi.files.item(i).size; 
+                    const file = Math.round((fsize / 1024)); 
+                    // The size of the file. 
+                    if (file > 2048) { 
+                        return True
+                        // alert("File too small, please select a file greater than 2mb"); 
+                    }else{
+                        return False
+                    }
+                } 
+            } 
 
         const isValid = form.checkValidity() && isValidName && isValidAge && isValidServiceOfficerName && isValidNric;
         if (!isValid) {
@@ -30,12 +52,15 @@ const Forms = (props) => {
             if (!isValidAge) errorsObj.formCustomerAge = `Must be at least 18 years old.`;
             if (!isValidServiceOfficerName) errorsObj.formServiceOfficerName = `Service Officer Name must not exceed 64 characters.`;
             if (!isValidNric) errorsObj.formNric = `NRIC must be in uppercase and only have 7 numeric numbers.`;
+            if (!isValidRegistrationTime) errorsObj.formRegistrationTime = "Registration time must be provided in { DD/MM/YYYY HH:mm:ss } format.";
+            if (!isValidBranchCode) errorsObj.formValidBranchCode = "Branch Code should be a valid DBS branch code.";
+            if (!isValidFileSize) errorsObj.formValidFileSize = "File attached should not exceed 2 megabytes"
             console.log(errorsObj)
             setErrors(errorsObj);
             return;
         }
         
-        const formObject = { customerName: formCustomerName, customerAge: formCustomerAge, serviceOfficerName: formServiceOfficerName, NRIC: formNric, image: file }
+        const formObject = { customerName: formCustomerName, customerAge: formCustomerAge, serviceOfficerName: formServiceOfficerName, NRIC: formNric, image: file, productType: [formProductType]}
         console.log(formObject)
     }
 
@@ -43,7 +68,7 @@ const Forms = (props) => {
         <Fragment>
             <section className="container">
                 <Form noValidate validated={validated} onSubmit={handleSubmit}>
-
+                    
                     <Form.Group controlId="formCustomerName">
                         <Form.Label>Customer Name</Form.Label>
                         <Form.Control type="text" placeholder="Customer Name" />
@@ -75,10 +100,10 @@ const Forms = (props) => {
                     </Form.Group>
 
                     <Form.Group>
-                        <Form.File id="exampleFormControlFile1" label="Upload Image" />
+                        <Form.File id="formUploadedFile" label="Upload Image" />
                     </Form.Group>
 
-                    <Form.Group controlId="exampleForm.SelectCustom">
+                    <Form.Group controlId="formProductType">
                         <Form.Label>Product Type</Form.Label>
                         <Form.Control as="select" custom>
                         <option>Select</option>
