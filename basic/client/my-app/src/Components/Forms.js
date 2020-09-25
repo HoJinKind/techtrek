b9fb08e883562b96d8282db3c3c9261fa610b40e
leeminhan <minhan.lmh@gmail.com>
2020-09-25 11:43:57 +0800

    Update to form component to get latest update from master

--- basic/client/my-app/src/Components/Forms.js
+++ basic/client/my-app/src/Components/Forms.js
@@ -8,24 +8,40 @@ const Forms = (props) => {
         <Fragment>
             <section className="container">
                 <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
+
+                    <Form.Group controlId="formCustomerName">
+                        <Form.Label>Customer Name</Form.Label>
+                        <Form.Control type="text" placeholder="Customer Name" />
                     </Form.Group>
 
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                    <Form.Group controlId="formCustomerAge">
+                        <Form.Label>Customer Age</Form.Label>
+                        <Form.Control type="text" placeholder="Customer Name" />
                     </Form.Group>
+
+                    <Form.Group controlId="formServiceOfficerName">
+                        <Form.Label>Service Officer Name</Form.Label>
+                        <Form.Control type="text" placeholder="Service Officer Name" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formNric">
+                        <Form.Label>Service Officer Name</Form.Label>
+                        <Form.Control type="text" placeholder="NRIC" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formNric">
+                        <Form.Label>NRIC</Form.Label>
+                        <Form.Control type="password" placeholder="NRIC" />
+                    </Form.Group>
+
                     <Form.Group controlId="formBasicCheckbox">
                         <Form.Check type="checkbox" label="Check me out" />
                     </Form.Group>
+
                     <Button variant="primary" type="submit">
                         Submit
                     </Button>
+                    
                 </Form>
             </section>
         </Fragment>
